e666762e6d6aacc07ddfb7fdd522f30199cfa798
Adrian Baumgart <adrian@abmgrt.dev>
2021-05-08 20:26:45 +0200

    added server snapshots

---
+++ Hetzner Cloud/Views/Subviews/Project/Servers/Snapshots/ProjectServerDetailSnapshotsView.swift
@@ -0,0 +1,23 @@
+//
+// Hetzner Cloud App (Hetzner Cloud)
+// File created by Adrian Baumgart on 08.05.21.
+//
+// Licensed under the MIT License
+// Copyright © 2020 Adrian Baumgart. All rights reserved.
+//
+// https://git.abmgrt.dev/exc_bad_access/hetznercloudapp-ios
+//
+
+import SwiftUI
+
+struct ProjectServerDetailSnapshotsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProjectServerDetailSnapshotsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProjectServerDetailSnapshotsView()
+    }
+}
