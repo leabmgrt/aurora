71455c9639e28c8585d81d2e7ce1672c0ed5bb36
Adrian Baumgart <adrian@abmgrt.dev>
2021-05-23 17:01:48 +0200

    cleaned up code in ProjectServerDetailViewController

--- Aurora/Views/Subviews/Project/Servers/ProjectServerDetailViewController.swift
+++ Aurora/Views/Subviews/Project/Servers/ProjectServerDetailViewController.swift
@@ -37,44 +37,44 @@ struct ProjectServerDetailView: View {
             Group {
                 Group {
                     HStack(alignment: .center) {
-                        Circle().foregroundColor(getServerStatusColor(controller.server.status)).frame(width: 25, height: 25, alignment: .center).shadow(color: getServerStatusColor(controller.server.status), radius: 3, x: 0, y: 0)
-                        Text("\(controller.server.status.rawValue)").bold()
+                        let serverStatus = controller.server.status
+                        let statusColor = getServerStatusColor(serverStatus)
+                        Circle().foregroundColor(statusColor).frame(width: 25, height: 25, alignment: .center).shadow(color: statusColor, radius: 3, x: 0, y: 0)
+                        Text("\(serverStatus.rawValue)").bold()
                         Spacer()
-                        if controller.server.protection.delete && controller.server.protection.rebuild {
+                        let protection = controller.server.protection
+                        if protection.delete && protection.rebuild {
                             Text("Locked").foregroundColor(.gray).italic()
                             Image(systemName: "lock").foregroundColor(.gray)
                         }
-                        /* Toggle(isOn: .constant(controller.server!.status != .off), label: {
-                             Text("")
-                         }) */
-
                     }.padding(.bottom)
                 }
                 LazyVGrid(columns: [GridItem(.adaptive(minimum: 300), alignment: .top)], alignment: .center, spacing: 10, pinnedViews: [], content: {
                     FloatingCardBackgroundView {
                         VStack {
+                            let serverType = controller.server.server_type
                             HStack {
-                                Text("Configuration (\(controller.server.server_type.name))").bold().font(.title3)
+                                Text("Configuration (\(serverType.name))").bold().font(.title3)
                                 Spacer()
                             }.padding(.bottom)
                             HStack {
                                 Image(systemName: "cpu")
-                                Text("\(Int(controller.server.server_type.cores)) VCPU\(controller.server.server_type.cores != 1 ? "s" : "")")
+                                Text("\(Int(serverType.cores)) VCPU\(serverType.cores != 1 ? "s" : "") (\(serverType.cpu_type.rawValue.lowercased()))")
                                 Spacer()
                             }
                             HStack {
                                 Image(systemName: "memorychip")
-                                Text("\(Int(controller.server.server_type.memory)) GB RAM")
+                                Text("\(Int(serverType.memory)) GB RAM")
                                 Spacer()
                             }
                             HStack {
                                 Image(systemName: "internaldrive")
-                                Text("\(Int(controller.server.server_type.disk)) GB DISK (\(controller.server.server_type.storage_type.rawValue.lowercased()))")
+                                Text("\(Int(serverType.disk)) GB DISK (\(serverType.storage_type.rawValue.lowercased()))")
                                 Spacer()
                             }
                             HStack {
                                 Image(systemName: "eurosign.circle")
-                                let pricing = controller.server.server_type.prices.first(where: { $0.location == controller.server.datacenter.location.name })!
+                                let pricing = serverType.prices.first(where: { $0.location == controller.server.datacenter.location.name })!
                                 Text("\(String(format: "%.2f", Double(pricing.price_monthly.gross)!))/mo")
                                 Spacer()
                             }
@@ -87,9 +87,9 @@ struct ProjectServerDetailView: View {
                                 Text("Network (Public)").bold().font(.title3)
                                 Spacer()
                             }.padding(.bottom)
-
-                            Text("IPv4: ") + Text("\(controller.server.public_net.ipv4.ip)").bold()
-                            Text("IPv6: ") + Text("\(controller.server.public_net.ipv6.ip)").bold()
+                            let publicNetwork = controller.server.public_net
+                            Text("IPv4: ") + Text("\(publicNetwork.ipv4.ip)").bold()
+                            Text("IPv6: ") + Text("\(publicNetwork.ipv6.ip)").bold()
                         }
                     }
 
@@ -99,10 +99,10 @@ struct ProjectServerDetailView: View {
                                 Text("Location").bold().font(.title3)
                                 Spacer()
                             }.padding(.bottom)
-
-                            Text("City: ") + Text("\(controller.server.datacenter.location.city)").bold()
-                            Text("Datacenter: ") + Text("\(controller.server.datacenter.description)").bold()
-                            Text("Country: ") + Text("\(controller.server.datacenter.location.country)").bold()
+                            let datacenter = controller.server.datacenter
+                            Text("City: ") + Text("\(datacenter.location.city)").bold()
+                            Text("Datacenter: ") + Text("\(datacenter.description)").bold()
+                            Text("Country: ") + Text("\(datacenter.location.country)").bold()
                         }
                     }
 
@@ -215,8 +215,6 @@ struct ProjectServerDetailView_Preview: PreviewProvider {
         NavigationView {
             ProjectServerDetailView(controller: .init(project: .example, server: .example))
         }.navigationViewStyle(StackNavigationViewStyle())
-            .previewDevice("iPad Pro (12.9-inch) (4th generation)")
-        // .preferredColorScheme(.dark)
     }
 }
 
